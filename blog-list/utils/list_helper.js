02ceec827ae6c0a07d98c97f79c243395ef096ec
JVPH <gyrospizzeria@protonmail.com>
2022-11-21 16:58:49 -0500

    Add solution to exercises 4.3.-4.7.

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -1,3 +1,6 @@
+// const { countBy, toPairs, flow, head, last, maxBy } = require('lodash')
+const _ = require('lodash')
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
@@ -20,15 +23,60 @@ const favoriteBlog = (listOfBlogs) => {
     return 0
   }
 
-  const arrOfLikes = listOfBlogs.map(blog => blog.likes)
+  const mostLikes = _.maxBy(listOfBlogs, 'likes')
 
-  const mostLikes = Math.max(...arrOfLikes)
+  return mostLikes
+}
 
-  return listOfBlogs.find(blog => blog.likes === mostLikes)
+const mostBlogs = (listOfBlogs) => {
+  if(listOfBlogs.length === 0){
+    return 0
+  }
+
+  const mostBlogs =  _
+    .chain(listOfBlogs)
+    .countBy('author')
+    .entries()
+    .maxBy(_.last)
+    .value()
+
+  const author = {
+    author: mostBlogs[0],
+    blogs: mostBlogs[1]
+  }
+
+  return author
+}
+
+const mostLikes = (listOfBlogs) => {
+  if(listOfBlogs.length === 0){
+    return 0
+  }
+
+  let likesPerAuthor = listOfBlogs.reduce((objAccumulator, { author, likes }) => {
+    objAccumulator[author] = objAccumulator[author] || 0
+    objAccumulator[author] += likes
+    return objAccumulator
+  },{})
+
+  const mostLikes = _
+    .chain(likesPerAuthor)
+    .entries()
+    .maxBy(_.last)
+    .value()
+
+  const authorWithMostLikes = {
+    author: mostLikes[0],
+    likes: mostLikes[1]
+  }
+
+  return authorWithMostLikes
 }
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
