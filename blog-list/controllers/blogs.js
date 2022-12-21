f34565b873996d151f122936f45f28778155bf2f
JVPH <gyrospizzeria@protonmail.com>
2022-12-21 11:47:37 -0500

    feat: Implement token-based authentication
    Adding new blogs is only possible if a valid token is sent with the HTTP POST request.

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -8,10 +9,26 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')){
+    return authorization.substring(7)
+  }
+  return null
+}
 
-  const user =  await User.findById(body.userId)
+blogsRouter.post('/', async (request, response) => {
+  // const body = request.body
+
+  // const user =  await User.findById(body.userId)
+
+  const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -23,7 +40,7 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
-  response.status(201).json(savedBlog)
+  response.json(savedBlog)``
 })
 
 blogsRouter.get('/:id', async (request, response) => {
